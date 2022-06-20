ac11265dc4d2329f627309c3de860f25af06ef77
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-06-20 12:15:36 +0400

    added host and port logging on start

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = 3000;
 
   const config = new DocumentBuilder()
     .setTitle('nest_app')
@@ -13,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port, () =>
+    console.log(`app running on http://localhost:${port}`),
+  );
 }
 bootstrap();
